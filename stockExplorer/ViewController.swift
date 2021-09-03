38e622c4ccabf1c5a29de189f96073ceb2f70987
wanoffski666 <petl-danila@mail.ru>
2021-09-04 01:48:10 +0300

    adding variants to picker

--- stockExplorer/ViewController.swift
+++ stockExplorer/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.companies.keys.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return Array(self.companies.keys)[row]
+    }
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var companyPicker: UIPickerView!
     @IBOutlet weak var companyName: UILabel!
+    private let companies: [String: String] = ["Apple": "APPL", "Microsoft": "MSFT", "Google": "GOOG", "Amazon": "AMZN", "Facebook": "FB"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.companyPicker.dataSource = self
+        self.companyPicker.delegate = self
         
     }
 
