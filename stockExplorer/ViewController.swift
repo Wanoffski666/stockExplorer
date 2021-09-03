3ce800cc1863c3b6896ea80d88145d15e94dc6a1
wanoffski666 <petl-danila@mail.ru>
2021-09-04 01:18:58 +0300

    adding 2 labels and outlet

--- stockExplorer/ViewController.swift
+++ stockExplorer/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var companyName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.companyName.text = "Apple"
     }
 
 
