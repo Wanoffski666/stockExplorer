35bcc56d41a56e6c7dab9d0f776f092d2e38e6bb
wanoffski666 <petl-danila@mail.ru>
2021-09-04 12:51:01 +0300

    adding JSON requests and start/stop animation of activity indicator

--- stockExplorer/ViewController.swift
+++ stockExplorer/ViewController.swift
@@ -24,12 +24,56 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var companyPicker: UIPickerView!
     @IBOutlet weak var companyName: UILabel!
     private let companies: [String: String] = ["Apple": "APPL", "Microsoft": "MSFT", "Google": "GOOG", "Amazon": "AMZN", "Facebook": "FB"]
+    
+    private func parseQuote(data: Data) {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with:data)
+            
+            guard
+                let json = jsonObject as? [String: Any],
+                let companyName = json["companyName"] as? String
+            else {
+                print("Invalid JSON format")
+                return
+            }
+            DispatchQueue.main.async {
+                self.displayStockInfo(companyName: companyName)
+            }
+        } catch {
+            print("JSON parsing error: " + error.localizedDescription)
+        }
+    }
+    
+    private func displayStockInfo(companyName: String) {
+        self.activityIndicator.stopAnimating()
+        self.companyName.text = companyName
+    }
+    
+    private func requestQuote(for symbol: String) {
+        let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote")!
+        let dataTask = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            guard
+                error == nil,
+                (response as? HTTPURLResponse)?.statusCode == 200,
+                let data = data
+            else {
+                print("Network error")
+                return
+            }
+            self.parseQuote(data: data)
+        }
+        dataTask.resume()
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.companyPicker.dataSource = self
         self.companyPicker.delegate = self
-        
+        self.activityIndicator.hidesWhenStopped = true
+        self.activityIndicator.startAnimating()
+        self.requestQuote(for: "APPL")
     }
 
 
